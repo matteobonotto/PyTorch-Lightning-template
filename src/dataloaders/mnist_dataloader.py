dfcb3e652fc837572807476c32739ebee4a100b3
matteobonotto <matteob.90@hotmail.it>
2024-03-01 23:13:37 +0100

    fixing dataloader

--- src/dataloaders/mnist_dataloader.py
+++ src/dataloaders/mnist_dataloader.py
@@ -7,6 +7,9 @@ from lightning import LightningDataModule
 from torch.utils.data import TensorDataset
 import pandas as pd
 from typing import Optional
+from helper_functions.data import read_h5_numpy
+
+
 # import matplotlib.pyplot as plt
 
 DTYPE = torch.float32
@@ -19,15 +22,11 @@ class UserTrainDataset(Dataset):
         self.path = path
 
         # load and prepare data
-        data = pd.read_csv(self.path, low_memory=False).to_numpy()
-        X,y = data[:,1:], data[:,0]
-        dims = X.shape
-        dims = [int(x) for x in [dims[0], np.sqrt(dims[1]), np.sqrt(dims[1])]]
-        X = X.reshape(dims)/255
-        # plt.imshow(X[10000,:,:])
-        # plt.show()
+        data = read_h5_numpy(self.path)
+        X,y = data['X'], data['y']
+
         self.X = torch.tensor(X,dtype=DTYPE).unsqueeze(1)
-        self.y = torch.tensor(y,dtype=torch.int64)
+        self.y = torch.tensor(y.ravel(),dtype=torch.int64)
 
     def __getitem__(self, idx):
         return self.X[idx, ...], self.y[idx, ...]
@@ -35,7 +34,6 @@ class UserTrainDataset(Dataset):
     def __len__(self):
         return self.y.shape[0]
     
-
 
 class UserLightningDataModule(LightningDataModule):
     def __init__(
@@ -66,6 +64,35 @@ class UserLightningDataModule(LightningDataModule):
     
     def test_dataloader(self):
         raise NotImplementedError
+    
+
+
+
+# class UserTrainDataset(Dataset):
+#     def __init__(self,path : str):
+#         super(UserTrainDataset,self).__init__()
+#         self.path = path
+
+#         # load and prepare data
+#         data = pd.read_csv(self.path, low_memory=False).to_numpy()
+#         X,y = data[:,1:], data[:,0]
+#         dims = X.shape
+#         dims = [int(x) for x in [dims[0], np.sqrt(dims[1]), np.sqrt(dims[1])]]
+#         X = X.reshape(dims)/255
+#         # plt.imshow(X[10000,:,:])
+#         # plt.show()
+#         self.X = torch.tensor(X,dtype=DTYPE).unsqueeze(1)
+#         self.y = torch.tensor(y,dtype=torch.int64)
+
+#     def __getitem__(self, idx):
+#         return self.X[idx, ...], self.y[idx, ...]
+    
+#     def __len__(self):
+#         return self.y.shape[0]
+    
+
+
+
 
 
 
